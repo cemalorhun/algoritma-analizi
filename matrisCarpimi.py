e7c833ab1a0a0946c6333ca8d9521b9e2990b49b
cemalorhun <c.orhunbkn@gmail.com>
2016-06-06 00:25:44 +0300

    Update matrisCarpimi.py

--- matrisCarpimi.py
+++ matrisCarpimi.py
@@ -17,37 +17,50 @@ def createMatris(satir_size,sutun_size):
 # -------------iki matris çarpimi---------------------------
 def matriscarp(matris1,matris2):
 	
-	for i in range(0,len(matris2)): # matris2'nin sütun sayısını bulmak için
-		sutun_sayisi = matris2[i]
+	for i in range(0,len(matris1)): # matris2'nin sütun sayısını bulmak için
+		sutun_sayisi = matris1[i]
+		
+	for i in range(0,len(matris2)): # matris1'nin sütun sayısını bulmak için
+		sutun_sayisi2 = matris2[i]
 	
 	matris3=[]
 	temp=[]
 	temp2=[]
 	
-	su = len(sutun_sayisi)	 #matris2 nin sütun sayısı	
+	su = len(sutun_sayisi)	 #matris1 nin sütun sayısı	
+	su2 = len(sutun_sayisi2) #matris2 nin sütun sayısı
 	sa = len(matris1)		 #matris1 in satır sayısı
 	sa2 = len(matris2)       #matris2 in satır sayısı
 	
-	for x in range(0,sa):
+	if su != sa2:
+		print("iki matrisin çarpılabilmesi icin ilk matrisin sutun sayisi ikinci matrisin satir sayisina esit olmalidir")
+	else:
+		for x in range(0,sa):
 		
-		for i in range(0,su):
-
-			for a in range(0,sa2):
-				temp.append(matris1[x][a]*matris2[a][i])
-			sayi = 0
-			for t in temp:
-				sayi = sayi + t
-			temp2.append(sayi)
-			temp = []
+			for i in range(0,su2):
+	
+				for a in range(0,sa2):
+					temp.append(matris1[x][a]*matris2[a][i])
+				sayi = 0
+				for t in temp:
+					sayi = sayi + t
+				temp2.append(sayi)
+				temp = []
 			
-	for a in range (0,sa):
-		temp=[]
-		for i in range (0,su):
-			kac = a*su + i
-			temp.append(temp2[kac])
-		matris3.append(temp)
-			
-	return matris3
+		for a in range (0,sa):
+			temp=[]
+			for i in range (0,su2):
+				kac = a*su2 + i
+				temp.append(temp2[kac])
+			matris3.append(temp)
+				
+		print("--matris carpimi--\n")	
+		for i in matris3:
+			print (i)
+			 
+		print("------------------\n")
+	
+	
 #---------------------------------------------------------				
 
 	
@@ -65,10 +78,7 @@ print("------------------\n")
 
 matris4 = matriscarp(matris1,matris2)	
 
-print("--matris carpimi--\n")	
-for i in matris4:
-    print (i)
-print("------------------\n")
+
   
             
                 
